10e2c24e0858aa26b30ff61beae0cad77e5e8ac5
Alexander Malikov <malikoff.at@gmail.com>
2024-11-07 19:21:21 +0300

    Изменение докерфайла

--- scripts/load_start.sh
+++ scripts/load_start.sh
@@ -25,7 +25,7 @@ apt-get update
 echo "Installing docker"
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-cd $HOME
+
 
 echo "Downloading runner from $RUNNER_URL"
 wget -O runner.zip "$RUNNER_URL" && unzip runner.zip;
@@ -33,15 +33,20 @@ wget -O runner.zip "$RUNNER_URL" && unzip runner.zip;
 echo 'Installing runner'
 mkdir $HOME/gitflic-runner;
 mkdir -p $HOME/gitflic-runner/build $HOME/gitflic-runner/cache $HOME/gitflic-runner/data/log
-cp runner.jar helper.jar $HOME/gitflic-runner
-cp shell-scripts/helper.sh $HOME/gitflic-runner
+cp ./runner.jar ./helper.jar $HOME/gitflic-runner
+cp ./shell-scripts/helper.sh $HOME/gitflic-runner
 sudo chmod +x $HOME/gitflic-runner/helper.sh
+cd $HOME/gitflic-runner
 
 echo 'Registering runner'
 java -jar runner.jar register --url "$GITFLIC_AGENT_URL" --registration-token "$GITFLIC_AGENT_TOKEN"
 
 echo 'Configuring runner'
 sed -i '1 i\runner.executor=shell\nlogging.file.name=data/log/server.log' config/application.properties
+
+echo "Downloading Firefox driver"
+wget -O driver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz && \
+tar -xf driver.tar.gz
 
 echo 'Starting runner'
 java -jar runner.jar start --config=config/application.properties
