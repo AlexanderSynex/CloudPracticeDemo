878ce40e68d1d7357c94279f6ec83fb0f94c1982
Alexander Malikov <malikoff.at@gmail.com>
2025-01-12 23:46:27 +0300

    Changed .ci

--- tests/functional_tests/test_index.py
+++ tests/functional_tests/test_index.py
@@ -10,7 +10,7 @@ driver = "/home/geckodriver" if "FirefoxWebDriver" not in os.environ.keys() else
 
 class TestBackend:
     def setup_method(self):
-        options = webdriver.firefox.webdriver.WebDriver()
+        driver = webdriver.Firefox(options=options)
         self.driver = webdriver.Remote(command_executor="http://localhost:4444",options=options)
 
     def test_add(self, url):
