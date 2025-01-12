a21e6fde54df08b24cab4aefd7cf874c14e49482
Alexander Malikov <malikoff.at@gmail.com>
2025-01-13 00:04:26 +0300

    Changed .ci

--- tests/functional_tests/test_index.py
+++ tests/functional_tests/test_index.py
@@ -11,6 +11,7 @@ driver = "/home/geckodriver" if "FirefoxWebDriver" not in os.environ.keys() else
 class TestBackend:
     def setup_method(self):
         options = webdriver.FirefoxOptions()
+        options.add_argument('--disable-blink-features=AutomationControlled')
         self.driver = webdriver.Remote(command_executor="http://localhost:4444",options=options)
 
     def test_add(self, url):
