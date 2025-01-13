45f7f73f50166c7ca9d82c14669b20aae067be44
Alexander Malikov <malikoff.at@gmail.com>
2025-01-14 00:31:53 +0300

    Changed .ci

--- tests/functional_tests/test_index.py
+++ tests/functional_tests/test_index.py
@@ -5,44 +5,26 @@ import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-# class TestBackend:
-#     def setup_method(self):
-#         # options = webdriver.FirefoxOptions()
-#         # options.add_argument("--headless")  # Run in headless mode
-#         # options.add_argument("--no-sandbox")
+class TestBackend:
+    def setup_method(self):
+        self.driver = webdriver.Remote(command_executor="http://selenium-chrome:4444/wd/hub",options=webdriver.ChromeOptions())
 
-#         self.driver = webdriver.Remote(command_executor="http://selenium-chrome:4444/wd/hub",options=webdriver.ChromeOptions())
+    def test_add(self, url):
+        print(f"Url is {url}")
+        self.driver.get(f'{url}/add/1&2')
+        assert "Add 1 and 2. Got 3!" == self.driver.find_element(By.TAG_NAME, "body").text
 
-#     def test_add(self, url):
-#         print(f"Url is {url}")
-#         self.driver.get(f'{url}/add/1&2')
-#         assert "Add 1 and 2. Got 3!" == self.driver.find_element(By.TAG_NAME, "body").text
+    def test_multiply(self, url):
+        self.driver.get(f'{url}/multiply/2&2')
+        assert "Multiply 2 and 2. Got 4!" == self.driver.find_element(By.TAG_NAME, "body").text
 
-#     def test_multiply(self, url):
-#         self.driver.get(f'{url}/multiply/2&2')
-#         assert "Multiply 2 and 2. Got 4!" == self.driver.find_element(By.TAG_NAME, "body").text
+    def test_divide(self, url):
+        self.driver.get(f'{url}/divide/10&2')
+        assert "Divide 10 and 2. Got 5.0!" == self.driver.find_element(By.TAG_NAME, "body").text
 
-#     def test_divide(self, url):
-#         self.driver.get(f'{url}/divide/10&2')
-#         assert "Divide 10 and 2. Got 5.0!" == self.driver.find_element(By.TAG_NAME, "body").text
+    def test_subtract(self, url):
+        self.driver.get(f'{url}/subtract/9&2')
+        assert "Subtract 9 and 2. Got 7!" == self.driver.find_element(By.TAG_NAME, "body").text
 
-#     def test_subtract(self, url):
-#         self.driver.get(f'{url}/subtract/9&2')
-#         assert "Subtract 9 and 2. Got 7!" == self.driver.find_element(By.TAG_NAME, "body").text
-
-#     def teardown_method(self, method):
-#         self.driver.close()
-
-class TestUntitled():
-  def setup_method(self, method):
-    self.driver = webdriver.Remote(command_executor="http://selenium-chrome:4444/wd/hub",options=webdriver.ChromeOptions())
-    self.vars = {}
-  
-  def teardown_method(self, method):
-    self.driver.quit()
-  
-  def test_untitled(self):
-    self.driver.get("http://testapp:5000/")
-    self.driver.set_window_size(1600, 876)
-    self.driver.find_element(By.CSS_SELECTOR, "body").click()
-  
+    def teardown_method(self, method):
+        self.driver.close()
