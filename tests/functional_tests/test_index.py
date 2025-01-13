9810dec2db0cea6890496cd20fa9a03989fb0c55
Alexander Malikov <malikoff.at@gmail.com>
2025-01-13 23:15:43 +0300

    Changed .ci

--- tests/functional_tests/test_index.py
+++ tests/functional_tests/test_index.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestBackend:
     def setup_method(self):
-        options = webdriver.ChromeOptions()
+        options = webdriver.FirefoxOptions()
         options.add_argument("--headless")  # Run in headless mode
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
