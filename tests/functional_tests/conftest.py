d7c59ec30a429830ac3491ca8855070dcf15a932
Alexander Malikov <malikoff.at@gmail.com>
2025-01-13 23:23:18 +0300

    Changed .ci

--- tests/functional_tests/conftest.py
+++ tests/functional_tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--url", action="store", default='http://testapp:5000')
+    parser.addoption("--url", action="store", default='http://172.17.0.3:5000')
 
 
 @pytest.fixture(scope='session')
