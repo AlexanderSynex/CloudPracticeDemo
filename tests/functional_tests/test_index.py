97db0088794f67432df4ec6ff54fc0e217bf228c
Alexander Malikov <malikoff.at@gmail.com>
2025-01-13 22:43:26 +0300

    Changed .ci

--- tests/functional_tests/test_index.py
+++ tests/functional_tests/test_index.py
@@ -5,8 +5,6 @@ import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-url = 
-
 class TestBackend:
     def setup_method(self):
         print(url)
